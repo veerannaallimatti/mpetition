99a96032672824b2d22e51902462b06d8d6e07cc
sagar <expie00101@gmail.com>
2013-04-27 19:10:42 +0530

    Changed the buildStuff.sh file. Signed-off-by: sagar <expie00101@gmail.com>

--- buildstuff.sh
+++ buildstuff.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 FAILED=0
-echo "build started: Path variable value "
+echo "build started: Displaying the current path"
 echo $PATH 
+echo "current working directory"
 echo `pwd`
 ls -al
+echo "changing the permission for $ANDROID_HOME"
 sudo chmod 777 -R ./android-sdk-linux/
 cd ./android-sdk-linux/
 ls -al
+echo "entering into MPetition"
+echo `pwd`
 cd ../MPetition
+echo `pwd`
+echo "Starting the ant to build"
 ant -f build.xml
 ant clean
 ant debug
